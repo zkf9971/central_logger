98b056c87c49ebd0a40814f213808023436db0b6
Pratik Khadloya <tispratik@gmail.com>
2011-07-28 15:44:29 -0700

    Fix NoMethodError: private method 'gsub' called for 500001487:Fixnum

--- lib/central_logger/mongo_logger.rb
+++ lib/central_logger/mongo_logger.rb
@@ -46,7 +46,7 @@ module CentralLogger
     def add(severity, message = nil, progname = nil, &block)
       if @level <= severity && message.present? && @mongo_record.present?
         # do not modify the original message used by the buffered logger
-        msg = logging_colorized? ? message.gsub(/(\e(\[([\d;]*[mz]?))?)?/, '').strip : message
+        msg = logging_colorized? ? message.to_s.gsub(/(\e(\[([\d;]*[mz]?))?)?/, '').strip : message
         @mongo_record[:messages][LOG_LEVEL_SYM[severity]] << msg
       end
       # may modify the original message
